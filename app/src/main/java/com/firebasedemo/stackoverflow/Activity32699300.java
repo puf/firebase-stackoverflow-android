5a5ceb53001852f9c642e885b4fc40e21d1b1779
Frank van Pufelen <frank.vanpuffelen@gmail.com>
2015-09-21 09:12:24 -0700

    Added activity for 32699300

---
+++ app/src/main/java/com/firebasedemo/stackoverflow/Activity32699300.java
@@ -0,0 +1,41 @@
+package com.firebasedemo.stackoverflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+/**
+ * This class is a minimal test case for the question on:
+ *
+ * http://stackoverflow.com/questions/32699300/retrieving-a-firebase-key-starting-with-strips-the-sign
+ *
+ */
+public class Activity32699300 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_32699300);
+
+        final TextView output = (TextView) findViewById(R.id.text32699300);
+
+        Firebase.setAndroidContext(this);
+        Firebase ref = new Firebase("https://stackoverflow.firebaseio.com/32699300/");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+               output.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+}
